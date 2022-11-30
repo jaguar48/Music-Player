416b6374018abc346bc160c98a31547ea2a18d0d
jaguar48 <okonkwostanley67@yahoo.com>
2022-11-30 13:22:11 +0100

    first

--- Music Player/Music.cs
+++ Music Player/Music.cs
@@ -1,42 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Music_Player
+﻿namespace Music_Player
 {
     internal class Music
     {
-            public List<MusicList> musics = new List<MusicList>{
-            new MusicList { Id = 1, Track ="May it be", Artist ="Enya" },
+        public static  List<MusicList> musics = new List<MusicList>{
+            new MusicList {Id = 1, Track ="May it be", Artist ="Enya" },
             new MusicList {Id = 2, Track ="Flora Secret", Artist ="Enya"},
             new MusicList {Id = 3,Track ="Orinoco flow", Artist ="Enya" },
             new MusicList {Id = 4, Track="Watermarks", Artist ="Enya"},
             new MusicList {Id = 5,Track ="China Roses",Artist ="Enya" },
             new MusicList {Id = 6,Track ="Storm in Africa",Artist ="Enya" } };
 
-            List<MusicList> GetMusic()
-            {
-                return musics;
-            }
+       static List<MusicList> GetMusic()
+        {
+            return musics;
+        }
 
-            public static void MusicDisplay()
-            {
-                Music mymusic = new Music();
-          
-                mymusic.GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
+        public static void MusicDisplay()
+        {
+            
 
-            }
+            GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
+
+        }
         public static void AddMusic()
         {
-            Music mymusic = new Music();
+            
 
             int myid = 0;
 
-            foreach (var msc in mymusic.GetMusic())
+            foreach (var msc in GetMusic())
             {
                 if (msc.Id != myid)
                 {
@@ -54,9 +46,8 @@ namespace Music_Player
                 Console.WriteLine("Enter Artist name");
                 var artist = Console.ReadLine();
 
-                mymusic.GetMusic().Add(new MusicList { Id = myid += 1, Track = track, Artist = artist });
-                mymusic.GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
-
+                GetMusic().Add(new MusicList { Id = myid += 1, Track = track, Artist = artist });
+                MusicDisplay();
                 Console.WriteLine("do you want to add more press 1 \n ");
                 var moremusic = Console.ReadLine();
                 if (moremusic == "1")
@@ -70,15 +61,16 @@ namespace Music_Player
                 }
             }
         }
-        public static void Removemusic() {
-            Music mymusic = new Music();
+        public static void Removemusic()
+        {
+            
 
             Console.WriteLine("enter music ID to remove");
             int removemsc = Convert.ToInt32(Console.ReadLine());
 
-            var firstMatch = mymusic.GetMusic().First(s => s.Id == removemsc);
-            mymusic.GetMusic().Remove(firstMatch);
-            mymusic.GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
+            var firstMatch = GetMusic().First(s => s.Id == removemsc);
+            GetMusic().Remove(firstMatch);
+            MusicDisplay();
 
         }
 
